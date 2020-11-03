121dd6ebdec4b19c25274a6b4a4867dc50c1754b
shuklaamar <amarshukla636@gmail.com>
2020-11-03 18:01:35 +0530

    before publishing2

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,30 +1,24 @@
 const users = []
 
 const addUser = ({ id, username, room }) => {
-    // Clean the data
     username = username.trim().toLowerCase()
     room = room.trim().toLowerCase()
 
-    // Validate the data
     if (!username || !room) {
         return {
-            error: 'Username and room are required!'
+            error: 'Username and room ?'
         }
     }
 
-    // Check for existing user
     const existingUser = users.find((user) => {
         return user.room === room && user.username === username
     })
-
-    // Validate username
     if (existingUser) {
         return {
             error: 'Username is in use!'
         }
     }
 
-    // Store user
     const user = { id, username, room }
     users.push(user)
     return { user }
